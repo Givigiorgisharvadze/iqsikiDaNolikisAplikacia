ba6fd0be0405e587aed18affe02c5700bfd2157a
Givigiorgisharvadze <wFML7pNEz5BSG2C>
2021-11-21 20:49:43 +0400

    TicTacToeApplication

---
+++ app/src/main/java/com/example/iqsikidanoliki_saregistracio/iqsikidanoliki_tamashissituacia.kt
@@ -0,0 +1,11 @@
+package com.example.iqsikidanoliki_saregistracio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class iqsikidanoliki_tamashissituacia : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_iqsikidanoliki_tamashissituacia)
+    }
+}
